a412598a708a4f44019455e43d18d2158d9b3e0e
Mykhailo Plokhin - GitLab <plokhmyk@student.cvut.cz>
2025-05-09 20:48:11 +0200

    initialized class log, and changed hibernate.hbm2ddl.auto from \validate to \update, to allow hibernates automatically create table if needed.

--- src/main/java/entities/Log.java
+++ src/main/java/entities/Log.java
@@ -1,9 +1,39 @@
 package entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "\"Log\"")
 public class Log {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // или другой способ генерации
+    private Long id;
+
+    private String message;
+
+    private String level;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
 }
